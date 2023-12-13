e9392b929d51d3d63c1a12ac371e5590a1d4f24f
Nico Schulterer <91744414+Nico-Schulterer@users.noreply.github.com>
2023-12-13 11:10:05 +0100

    Added parent class card, children classes cards as well nothing major yet....

---
+++ Mobile_CCG/Assets/DreamBorne/Scripts/Cards/Guards.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guards : Guardian
+{
+
+    private int power;
+
+
+    public void Effect()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Mobile_CCG/Assets/DreamBorne/Scripts/Cards/Raider.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raider : Card
+{
+    private int cost;
+    private int power;
+
+
+    public void onReveal()
+    {
+
+    }
+
+    public void OnDie()
+    {
+        
+    }
+
+    public void OnGoing()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
